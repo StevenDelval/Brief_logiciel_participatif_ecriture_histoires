f3600f68a9f85a1bd6eb0fbc022bbb0f8a8a6ce9
kahoul yanis <kahoul.yanis@gmail.com>
2022-07-05 17:05:20 +0200

    add actions user

--- main.py
+++ main.py
@@ -1,8 +1,10 @@
-import os
+
 
 import CRUD
 
-have_account = bool(int(input("Avez-vous un compte\n0 = Non\n1 = Oui\n")))
+# Connexion utilisateur 
+
+""" have_account = bool(int(input("Avez-vous un compte\n0 = Non\n1 = Oui\n")))
 connected = False
 count = 0
 while not connected:
@@ -36,4 +38,30 @@ while not connected:
                 print("Erreur !")
         else:
             have_account = bool(int(input("Avez-vous un compte\n0 = Non\n1 = Oui\n")))
+
+ """
+# afficher_dernier_paragraphe
+# curseur.lastrowid
+
+
+
+actions = int(input("que voulez vous faire \n  \n 0 = ecrire un paragraphe? \n 1 = ecrire un commentaire? \n 2 = lancer un vote?  \n 3 = creer un nouveau chapitre?  \n 4 = voir un résumé de chapitre? \n 5 = voter?\n 6 = voir perso? \n 7= nouv chap? \n 8= delete chapter \n "))
+
+if actions == 0:
+    paragraph = input (" Rentrez votre contribution: ")
+    CRUD.create_paragraph(paragraph)
+
+elif actions == 1 :
+    commentaire = input (" Rentrez votre contribution: ")
+    CRUD.create_commentaire(commentaire)
+elif actions == 2:
+    start_poll
+elif actions ==3:
+    new_chapter = input (" Rentrez votre contribution: ")
+    CRUD.create_paragraph(new_chapter)
+elif actions == 4:
+    see_resume
+elif 
+
+
 
