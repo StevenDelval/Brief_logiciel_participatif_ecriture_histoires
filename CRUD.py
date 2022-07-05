1ec692867997de17bcfe3f9b6e457a482aaf768c
Machtelinck <subaruparia@gmail.com>
2022-07-05 10:05:23 +0200

    Create function create_user and see_user

--- CRUD.py
+++ CRUD.py
@@ -1,10 +1,24 @@
+import sqlite3 
+
 #### Create ####
+def create_user(Username, Password):
+    connexion = sqlite3.connect("bdd.db")
+    curseur = connexion.cursor()
+    curseur.execute("INSERT INTO User VALUES (?,?,?);", (None,Username, Password))
+    connexion.commit()
+    connexion.close()
 
 
 
 #### Read   ####
 
-
+def see_users():
+    connexion = sqlite3.connect("bdd.db")
+    curseur = connexion.cursor()
+    curseur.execute("SELECT Username FROM User")
+    print(curseur.fetchall())
+    connexion.close()
+    
 
 #### Update ####
 
