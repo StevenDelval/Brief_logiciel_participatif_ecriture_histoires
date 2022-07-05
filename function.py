2d40d4b345ebb94d241bd4967c5f1ecd0c3d74fb
StevenDelval <asteven59220@free.fr>
2022-07-05 14:59:38 +0200

    Add docstring function crypt in function.py

--- function.py
+++ function.py
@@ -2,6 +2,11 @@ import hashlib
 
 
 def crypt(password):
+    """
+    Fonction qui crypte un mot de passe
+    :param password (str): mot de passe
+    :return (str) le hash du mot de passe
+    """
     hash_pwd = hashlib.new('sha256')
     hash_pwd.update(password.encode())
     hash_pwd = hash_pwd.hexdigest()
