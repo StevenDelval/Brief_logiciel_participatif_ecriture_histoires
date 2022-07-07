ddebabeeb9385531ff806d1ac14b2acdbd049e9d
henzozhang <zhangleon1985@gmail.com>
2022-07-07 16:18:56 +0200

    ajouter read_comment

--- ecrire la suite zhang.py
+++ ecrire la suite zhang.py
@@ -2,11 +2,11 @@
 import function
 import CRUD
 import sqlite3 
-import hashlib
+
 connexion = sqlite3.connect("Chinook_Sqlite.sqlite")
 curseur = connexion.cursor()
 
-clear_terminal()
+function.clear_terminal()
 
 last_chapter = CRUD.find_id_last_chapter
 print("Chapitre"+last_chapter)
@@ -14,8 +14,8 @@ print(CRUD.read_chapter(last_chapter))
 print("----------------------")
 print("Liste des personnages:"+CRUD.read_personnage(last_chapter))
 print("----------------------")
-print("dernier message:"+)
+print("dernier message:"+CRUD.read_comment(last_chapter))
+print("posté par:")
 
 
 
-input 
