6d7c73da4fc6acb4c0bdf7b464bcf20baebc445e
StevenDelval <asteven59220@free.fr>
2022-07-07 10:18:04 +0200

    Add function print_paragraph and find_index_chapter

--- function.py
+++ function.py
@@ -10,3 +10,22 @@ def clear_terminal():
         os.system('clear')
     else:
         os.system('cls')
+
+def print_paragraph(index,paragraphs):
+    clear_terminal()
+    if index < len(paragraphs) :
+        print("""Chapitre {0} \n
+        Resumer :\n
+        {1}\n
+        __________________
+        {2}\n
+        """.format(paragraphs[index][0],paragraphs[index][1],paragraphs[index][2]))
+    else :
+        print("Il n'y a plus de paragraphe\n")
+        
+def find_index_chapter(Chapter,story):
+    n = 0
+    for paragraph in story:
+        if paragraph[0] == Chapter:
+            return n
+        n +=1
