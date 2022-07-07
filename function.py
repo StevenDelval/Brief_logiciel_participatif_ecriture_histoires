578d24a127cdd02f7e08085c680673db0e218b29
StevenDelval <asteven59220@free.fr>
2022-07-07 10:22:24 +0200

    Add docstring

--- function.py
+++ function.py
@@ -12,6 +12,11 @@ def clear_terminal():
         os.system('cls')
 
 def print_paragraph(index,paragraphs):
+    """
+    Fonction qui affiche le numero de chapitre, le resumer un paragraphe
+    :param index(int): numero de paragraphe
+    :param paragraphs(list): liste des paragraphes 
+    """
     clear_terminal()
     if index < len(paragraphs) :
         print("""Chapitre {0} \n
@@ -24,6 +29,9 @@ def print_paragraph(index,paragraphs):
         print("Il n'y a plus de paragraphe\n")
         
 def find_index_chapter(Chapter,story):
+    """
+    Fonction qui renvoie l'index du premier paragraphes du chapitre choisi
+    """
     n = 0
     for paragraph in story:
         if paragraph[0] == Chapter:
